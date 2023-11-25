5d800b6139de6f3404deb8fed3a5d246cc75ec29
Timur010 <duakar010@gmail.com>
2023-11-25 19:24:34 +0300

    Initial Commit

---
+++ CryptoKad/CryptoKadApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoKadApp.swift
+//  CryptoKad
+//
+//  Created by Timur Kadiev on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoKadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
